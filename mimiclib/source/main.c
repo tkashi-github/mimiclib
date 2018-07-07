80ba38d2658bc87207ce3ce23d8de4acf9d713bc
Takashi Kashiwagi <tkashi.al@outlook.com>
2018-07-07 18:38:12 +0900

    cmake動いてない

--- mimiclib/source/main.c
+++ mimiclib/source/main.c
@@ -7,6 +7,6 @@
 int main(argc, *argv[]){
 
 	// Addo Unit Test Here
-
+	mimic_printf("mimi_c unit test\r\n");
 	return 0;
 }
--- mimiclib/source/mimicio.c
+++ mimiclib/source/mimicio.c
@@ -10,19 +10,24 @@
 #define INCG_MIMICIO_H
 
 #include "mimicio.h"
+#ifdef DWIN_TEST
 #include <stdio.h>	//	TODO íœ‚·‚é
 #include <stdlib.h>	//	TODO íœ‚·‚é
-
+#endif
 
 void mimic_printf(const char* fmt, ...){
 	va_list arg;
 	char szBuffer[1024];
 
 	va_start(arg, fmt);
+#ifdef DWIN_TEST
 	vsnprintf(szBuffer, sizeog(szBuffer), fmt, arg);
+#endif
 	va_end(arg);
 	
+#ifdef DWIN_TEST
 	puts(szBuffer);
+#endif
 }
 
 #endif
