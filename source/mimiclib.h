5ea90fe03eebdd16e177381f0b53e242bfbe4aa3
Takashi Kashiwagi <tkashi.al@outlook.com>
2019-08-07 21:49:53 +0900

    staticfunctionを*.cに一部移動

--- source/mimiclib.h
+++ source/mimiclib.h
@@ -92,7 +92,7 @@ void MIMICLIB_GetChar(TCHAR *ch);
  * @param [in] ch character
  * @return void
  */
-	void MIMICLIB_PutChar(TCHAR ch);
+void MIMICLIB_PutChar(TCHAR ch);
 
 /**
  * @brief puts (with Semapore)
@@ -355,49 +355,7 @@ static inline _Bool mimic_memset(uintptr_t p1, uint8_t val, uint32_t u32ByteCnt)
  * @param [in] ctx 
  * @return char* 
  */
-static inline char *mimic_strtok(char szStr[], const uint32_t SizeOfStr, const char szDelm[], const uint32_t SizeOfDelm, char **ctx)
-{
-	/*-- var --*/
-	char *pret = NULL;
-
-	/*-- begin --*/
-	if (szDelm != (char *)NULL)
-	{
-		char *pszTmp;
-		uint32_t u32 = 0u;
-		uint32_t delmLen = mimic_strlen(szDelm, SizeOfDelm);
-
-		if (szStr == NULL)
-		{
-			pszTmp = *ctx;
-		}
-		else
-		{
-			pszTmp = szStr;
-		}
-		if (pszTmp[0] != '\0')
-		{
-			pret = pszTmp;
-			while (pszTmp[u32] != '\0')
-			{
-				*ctx = &pszTmp[u32 + delmLen];
-				if (mimic_memcmp((uintptr_t)&pszTmp[u32], (uintptr_t)szDelm, delmLen) != false)
-				{
-					pszTmp[u32] = '\0';
-					pret = pszTmp;
-					break;
-				}
-				u32++;
-				if(u32 >= SizeOfStr)
-				{
-					break;
-				}
-			}
-		}
-	}
-	return pret;
-}
-
+extern char *mimic_strtok(char szStr[], const uint32_t SizeOfStr, const char szDelm[], const uint32_t SizeOfDelm, char **ctx);
 /**
  * @brief tcstok_r
  * @param [in] szStr Target String
@@ -405,44 +363,7 @@ static inline char *mimic_strtok(char szStr[], const uint32_t SizeOfStr, const c
  * @param [in] ctx 
  * @return char* 
  */
-static inline char *mimic_tcstok(TCHAR szStr[], const TCHAR szDelm[], const uint32_t SizeOfDelm, TCHAR **ctx)
-{
-	/*-- var --*/
-	TCHAR *pret = NULL;
-
-	/*-- begin --*/
-	if (szDelm != NULL)
-	{
-		TCHAR *pszTmp;
-		uint32_t u32 = 0u;
-		uint32_t delmLen = mimic_tcslen(szDelm, SizeOfDelm);
-
-		if (szStr == NULL)
-		{
-			pszTmp = *ctx;
-		}
-		else
-		{
-			pszTmp = szStr;
-		}
-		if (pszTmp[0] != (TCHAR)'\0')
-		{
-			pret = pszTmp;
-			while (pszTmp[u32] != (TCHAR)'\0')
-			{
-				*ctx = &pszTmp[u32 + delmLen];
-				if (mimic_memcmp((uintptr_t)&pszTmp[u32], (uintptr_t)szDelm, sizeof(TCHAR) * delmLen) != false)
-				{
-					pszTmp[u32] = (TCHAR)'\0';
-					pret = pszTmp;
-					break;
-				}
-				u32++;
-			}
-		}
-	}
-	return pret;
-}
+extern TCHAR *mimic_tcstok(TCHAR szStr[], const TCHAR szDelm[], const uint32_t SizeOfDelm, TCHAR **ctx);
 
 /**
  * @brief isprint
@@ -613,56 +534,7 @@ static inline enRetrunCodeStrCmp_t mimic_tcsncmp(const TCHAR szStr1[], const TCH
  * @param [in] u32MaxElementOfszDst (!= 0)
  * @return TCHAR * (szDst)
  */
-static inline TCHAR *mimic_ltoa(const int32_t i32Val, TCHAR szDst[], uint32_t u32MaxElementOfszDst)
-{
-	uint32_t u32Index = 0;
-	int32_t i32Sign;
-	uint32_t u32Val;
-
-	if ((szDst == NULL) || (u32MaxElementOfszDst == 0))
-	{
-		return NULL;
-	}
-
-	i32Sign = i32Val;
-	if (i32Sign < 0)
-	{
-		u32Val = (uint32_t)-i32Val;
-	}
-	else
-	{
-		u32Val = (uint32_t)i32Val;
-	}
-	mimic_memset((uintptr_t)szDst, 0, sizeof(TCHAR) * u32MaxElementOfszDst);
-
-	while (u32Index < (u32MaxElementOfszDst - 1))
-	{
-		uint32_t u32 = (u32Val % 10);
-		u32Val /= 10;
-		szDst[u32Index] = (TCHAR)u32 + (TCHAR)'0';
-		u32Index++;
-		if (u32Val == 0)
-		{
-			break;
-		}
-	}
-
-	/* Add sign */
-	if (i32Sign < 0)
-	{
-		szDst[u32Index] = (TCHAR)'-';
-		u32Index++;
-	}
-
-	/* Reverse */
-	for (uint32_t j = 0; j < (u32Index / 2); j++)
-	{
-		TCHAR tcTemp = szDst[j];
-		szDst[j] = szDst[u32Index - 1 - j];
-		szDst[u32Index - 1 - j] = tcTemp;
-	}
-	return szDst;
-}
+extern TCHAR *mimic_ltoa(const int32_t i32Val, TCHAR szDst[], uint32_t u32MaxElementOfszDst);
 
 /**
  * @brief ultoa
@@ -672,47 +544,7 @@ static inline TCHAR *mimic_ltoa(const int32_t i32Val, TCHAR szDst[], uint32_t u3
  * @param [in] u32Radix (!= 0)
  * @return TCHAR * (szDst)
  */
-static inline TCHAR *mimic_ultoa(const uint32_t u32Val, TCHAR szDst[], const uint32_t u32MaxElementOfszDst, const uint32_t u32Radix)
-{
-	uint32_t u32Index = 0;
-	uint32_t u32Temp = u32Val;
-
-	if ((szDst == NULL) || (u32MaxElementOfszDst == 0) || (u32Radix == 0))
-	{
-		return NULL;
-	}
-
-	mimic_memset((uintptr_t)szDst, 0, sizeof(TCHAR) * u32MaxElementOfszDst);
-
-	while (u32Index < (u32MaxElementOfszDst - 1))
-	{
-		uint32_t u32 = (u32Temp % u32Radix);
-		u32Temp /= u32Radix;
-
-		if (u32 <= 9)
-		{
-			szDst[u32Index] = (TCHAR)u32 + (TCHAR)'0';
-		}
-		else
-		{
-			szDst[u32Index] = (TCHAR)(u32 - 10) + (TCHAR)'A';
-		}
-		u32Index++;
-		if (u32Temp == 0)
-		{
-			break;
-		}
-	}
-
-	/* Reverse*/
-	for (uint32_t j = 0; j < (u32Index / 2); j++)
-	{
-		TCHAR tcTemp = szDst[j];
-		szDst[j] = szDst[u32Index - 1 - j];
-		szDst[u32Index - 1 - j] = tcTemp;
-	}
-	return szDst;
-}
+extern TCHAR *mimic_ultoa(const uint32_t u32Val, TCHAR szDst[], const uint32_t u32MaxElementOfszDst, const uint32_t u32Radix);
 
 /**
  * @brief lltoa
@@ -721,55 +553,7 @@ static inline TCHAR *mimic_ultoa(const uint32_t u32Val, TCHAR szDst[], const uin
  * @param [in] u32MaxElementOfszDst (!= 0)
  * @return TCHAR * (szDst)
  */
-static inline TCHAR *mimic_lltoa(const int64_t i64Val, TCHAR szDst[], const uint32_t u32MaxElementOfszDst)
-{
-	uint32_t u32Index = 0;
-	int64_t i64Sign;
-	uint64_t u64Val;
-
-	if ((szDst == NULL) || (u32MaxElementOfszDst == 0))
-	{
-		return NULL;
-	}
-	i64Sign = i64Val;
-	if (i64Sign < 0)
-	{
-		u64Val = (uint64_t)-i64Val;
-	}
-	else
-	{
-		u64Val = (uint64_t)i64Val;
-	}
-	mimic_memset((uintptr_t)szDst, 0, sizeof(TCHAR) * u32MaxElementOfszDst);
-
-	while (u32Index < (u32MaxElementOfszDst - 1))
-	{
-		uint32_t u32 = (uint32_t)(u64Val % 10ull);
-		u64Val /= 10ull;
-		szDst[u32Index] = (TCHAR)u32 + (TCHAR)'0';
-		u32Index++;
-		if (u64Val == 0)
-		{
-			break;
-		}
-	}
-
-	/* Add sign */
-	if (i64Sign < 0)
-	{
-		szDst[u32Index] = (TCHAR)'-';
-		u32Index++;
-	}
-
-	/* Reverse*/
-	for (uint32_t j = 0; j < (u32Index / 2); j++)
-	{
-		TCHAR tcTemp = szDst[j];
-		szDst[j] = szDst[u32Index - 1 - j];
-		szDst[u32Index - 1 - j] = tcTemp;
-	}
-	return szDst;
-}
+extern TCHAR *mimic_lltoa(const int64_t i64Val, TCHAR szDst[], const uint32_t u32MaxElementOfszDst);
 
 /**
  * @brief ulltoa
@@ -779,48 +563,9 @@ static inline TCHAR *mimic_lltoa(const int64_t i64Val, TCHAR szDst[], const uint
  * @param [in] u32Radix (!= 0)
  * @return TCHAR * (szDst)
  */
-static inline TCHAR *mimic_ulltoa(const uint64_t u64Val, TCHAR szDst[], const uint32_t u32MaxElementOfszDst, const uint32_t u32Radix)
-{
-	uint32_t u32Index = 0;
-	uint64_t u64Temp = u64Val;
+extern TCHAR *mimic_ulltoa(const uint64_t u64Val, TCHAR szDst[], const uint32_t u32MaxElementOfszDst, const uint32_t u32Radix);
 
-	if ((szDst == NULL) || (u32MaxElementOfszDst == 0) || (u32Radix == 0))
-	{
-		return NULL;
-	}
-
-	mimic_memset((uintptr_t)szDst, 0, sizeof(TCHAR) * u32MaxElementOfszDst);
-
-	while (u32Index < (u32MaxElementOfszDst - 1))
-	{
-		uint32_t u32 = (uint32_t)(u64Temp % u32Radix);
-		u64Temp /= u32Radix;
-		if (u32 < 10)
-		{
-			szDst[u32Index] = (TCHAR)u32 + (TCHAR)'0';
-		}
-		else
-		{
-			szDst[u32Index] = (TCHAR)(u32 - 10u) + (TCHAR)'A';
-		}
-		u32Index++;
-		if (u64Temp == 0)
-		{
-			break;
-		}
-	}
-
-	/* Reverse*/
-	for (uint32_t j = 0; j < (u32Index / 2); j++)
-	{
-		TCHAR tcTemp = szDst[j];
-		szDst[j] = szDst[u32Index - 1 - j];
-		szDst[u32Index - 1 - j] = tcTemp;
-	}
-	return szDst;
-}
-
-	/**
+/**
  * @brief tcscat
  * @param [inout] pszStr1 (!= NULL)
  * @param [in] u32MaxElementOfszStr1 (!= 0)
@@ -828,37 +573,7 @@ static inline TCHAR *mimic_ulltoa(const uint64_t u64Val, TCHAR szDst[], const ui
  * @return TCHAR * (pszStr1)
  * @return NULL argment NG
  */
-static inline TCHAR *mimic_tcscat(TCHAR pszStr1[], uint32_t u32MaxElementOfszStr1, const TCHAR pszStr2[])
-{
-	uint32_t u32Pos = 0;
-	uint32_t u32Index = 0;
-
-	if ((pszStr1 == (TCHAR *)NULL) || (u32MaxElementOfszStr1 == 0) || (pszStr2 == 0))
-	{
-		return NULL;
-	}
-	while (pszStr1[u32Pos] != (TCHAR)'\0')
-	{
-		u32Pos++;
-		if (u32Pos >= (u32MaxElementOfszStr1 - 1))
-		{
-			return pszStr1;
-		}
-	}
-
-	while (pszStr2[u32Index] != (TCHAR)'\0')
-	{
-		pszStr1[u32Pos] = pszStr2[u32Index];
-		u32Pos++;
-		u32Index++;
-		if (u32Pos >= (u32MaxElementOfszStr1 - 1))
-		{
-			return pszStr1;
-		}
-	}
-
-	return pszStr1;
-}
+extern TCHAR *mimic_tcscat(TCHAR pszStr1[], uint32_t u32MaxElementOfszStr1, const TCHAR pszStr2[]);
 
 /**
  * @brief ftoa
@@ -870,76 +585,8 @@ static inline TCHAR *mimic_tcscat(TCHAR pszStr1[], uint32_t u32MaxElementOfszStr
  * @return TCHAR * (pszStr1)
  * @return NULL argment NG
  */
-static inline TCHAR *mimic_ftoa(const double dfpVal, TCHAR szDst[], const uint32_t u32MaxElementOfszDst, const uint32_t precision_width)
-{
-	double dfpTemp = dfpVal;
-	uint64_t u64Z;
-	uint32_t u32Pos = 0;
-	uint32_t u32PrecCnt = 0;
+extern TCHAR *mimic_ftoa(const double dfpVal, TCHAR szDst[], const uint32_t u32MaxElementOfszDst, const uint32_t precision_width);
 
-	if ((szDst == (TCHAR *)NULL) || (u32MaxElementOfszDst == 0) || (precision_width == 0))
-	{
-		return NULL;
-	}
-
-	mimic_memset((uintptr_t)szDst, 0, sizeof(TCHAR) * u32MaxElementOfszDst);
-	if (dfpVal < 0.0)
-	{
-		dfpTemp = -dfpVal;
-	}
-
-	u64Z = dfpTemp;
-	dfpTemp -= (double)u64Z;
-
-	if (u64Z != 0)
-	{
-		if (dfpVal < 0.0)
-		{
-			szDst[0] = (TCHAR)'-';
-			mimic_ulltoa(u64Z, &szDst[1], u32MaxElementOfszDst - 1, 10);
-		}
-		else
-		{
-			mimic_ulltoa(u64Z, szDst, u32MaxElementOfszDst, 10);
-		}
-
-		u32Pos = mimic_tcslen(szDst, u32MaxElementOfszDst);
-	}
-	else
-	{
-		if (dfpVal < 0.0)
-		{
-			szDst[u32Pos] = (TCHAR)'-';
-			u32Pos++;
-		}
-		szDst[u32Pos] = (TCHAR)'0';
-		u32Pos++;
-	}
-	szDst[u32Pos] = (TCHAR)'.';
-	u32Pos++;
-	u32PrecCnt = 0;
-
-	for (;;)
-	{
-		dfpTemp *= 10.0;
-		if (u32PrecCnt >= (precision_width - 1))
-		{
-			dfpTemp += 0.5;
-		}
-		uint32_t u32Z = (uint32_t)dfpTemp;
-		dfpTemp -= (double)u32Z;
-
-		szDst[u32Pos] = (TCHAR)u32Z + (TCHAR)'0';
-		u32Pos++;
-		u32PrecCnt++;
-		if ((dfpTemp == 0.0) || (u32PrecCnt >= precision_width))
-		{
-			break;
-		}
-	}
-
-	return szDst;
-}
 
 /**
  * @brief atof
@@ -947,73 +594,7 @@ static inline TCHAR *mimic_ftoa(const double dfpVal, TCHAR szDst[], const uint32
  * @param [in] u32BufSize sizeof(szStr) (!= 0)
  * @return double
  */
-static inline double mimic_atof(const TCHAR szStr[], const uint32_t u32BufSize)
-{
-	volatile double dfp = 0.0;
-	uint32_t i = 0;
-	int32_t sign = 1;
-
-	if ((szStr == NULL) || (u32BufSize == 0u))
-	{
-		return 0.0;
-	}
-
-	while (szStr[i] == ' ')
-	{
-		i++;
-		if (i >= u32BufSize)
-		{
-			return 0.0;
-		}
-	}
-
-	if (szStr[i] == '-')
-	{
-		i++;
-		sign = -1;
-	}
-
-	while ((szStr[i] != '\0') && (i < u32BufSize))
-	{
-		if ((szStr[i] >= '0') && (szStr[i] <= '9'))
-		{
-			uint32_t tmp = (uint32_t)(szStr[i] - '0');
-			dfp *= 10;
-			dfp += tmp;
-		}
-		else if (szStr[i] == '.')
-		{
-			i++;
-			break;
-		}
-		else
-		{
-			/* NOP */
-		}
-		i++;
-	}
-
-	{
-		uint32_t u32DecimalPoint = 1u;
-		volatile double dfp2 = 0.0;
-		while ((szStr[i] != '\0') && (i < u32BufSize))
-		{
-			if ((szStr[i] >= '0') && (szStr[i] <= '9'))
-			{
-				uint32_t tmp = (uint32_t)(szStr[i] - '0');
-				u32DecimalPoint *= 10;
-				dfp2 += (double)tmp / (double)u32DecimalPoint;
-			}
-			else
-			{
-				break;
-			}
-			i++;
-		}
-		dfp += dfp2;
-	}
-	return sign * (dfp);
-}
+extern double mimic_atof(const TCHAR szStr[], const uint32_t u32BufSize);
 
 /**
  * @brief strtol
@@ -1021,56 +602,7 @@ static inline double mimic_atof(const TCHAR szStr[], const uint32_t u32BufSize)
  * @param [in] u32BufSize sizeof(szStr) (!= 0)
  * @return int32_t
  */
-static inline int32_t mimic_strtol(const TCHAR szStr[], const uint32_t u32BufSize)
-{
-	int32_t iret = 0;
-	uint32_t u32Val = 0;
-	uint32_t i = 0;
-	int32_t sign = 1;
-
-	if ((szStr == NULL) || (u32BufSize == 0u))
-	{
-		return 0;
-	}
-
-	while (szStr[i] == ' ')
-	{
-		i++;
-		if (i >= u32BufSize)
-		{
-			return 0;
-		}
-	}
-
-	if (szStr[i] == '-')
-	{
-		i++;
-		sign = -1;
-	}
-
-	while ((szStr[i] != '\0') && (i < u32BufSize))
-	{
-		if ((szStr[i] >= '0') && (szStr[i] <= '9'))
-		{
-			uint32_t tmp = (uint32_t)(szStr[i] - '0');
-			u32Val *= 10;
-			u32Val += tmp;
-		}
-		else
-		{
-			/* NOP */
-		}
-		i++;
-	}
-
-	if (u32Val >= INT32_MAX)
-	{
-		iret = INT32_MAX;
-	}
-
-	iret = (int32_t)u32Val;
-	return sign * iret;
-}
+extern int32_t mimic_strtol(const TCHAR szStr[], const uint32_t u32BufSize);
 
 /**
  * @brief strtol
@@ -1078,56 +610,7 @@ static inline int32_t mimic_strtol(const TCHAR szStr[], const uint32_t u32BufSiz
  * @param [in] u32BufSize sizeof(szStr) (!= 0)
  * @return int64_t
  */
-static inline int64_t mimic_strtoll(const TCHAR szStr[], const uint32_t u32BufSize)
-{
-	int64_t iret = 0;
-	uint64_t u64Val = 0;
-	uint32_t i = 0;
-	int32_t sign = 1;
-
-	if ((szStr == NULL) || (u32BufSize == 0u))
-	{
-		return 0;
-	}
-
-	while (szStr[i] == ' ')
-	{
-		i++;
-		if (i >= u32BufSize)
-		{
-			return 0;
-		}
-	}
-
-	if (szStr[i] == '-')
-	{
-		i++;
-		sign = -1;
-	}
-
-	while ((szStr[i] != '\0') && (i < u32BufSize))
-	{
-		if ((szStr[i] >= '0') && (szStr[i] <= '9'))
-		{
-			uint64_t tmp = (uint64_t)(szStr[i] - '0');
-			u64Val *= 10;
-			u64Val += tmp;
-		}
-		else
-		{
-			/* NOP */
-		}
-		i++;
-	}
-
-	if (u64Val >= INT64_MAX)
-	{
-		iret = INT64_MAX;
-	}
-
-	iret = (int64_t)u64Val;
-	return sign * iret;
-}
+extern int64_t mimic_strtoll(const TCHAR szStr[], const uint32_t u32BufSize);
 
 /**
  * @brief strtoul
@@ -1136,52 +619,7 @@ static inline int64_t mimic_strtoll(const TCHAR szStr[], const uint32_t u32BufSi
  * @param [in] u32BufSize sizeof(szStr) (!= 0)
  * @return uint32_t
  */
-static inline uint32_t mimic_strtoul(const TCHAR szStr[], const uint32_t u32BufSize, const uint32_t u32Base)
-{
-	uint32_t u32Val = 0;
-	uint32_t i = 0;
-	if ((szStr == NULL) || (u32BufSize == 0u) || ((u32Base != 10u) && (u32Base != 16u)))
-	{
-		return 0;
-	}
-	while (szStr[i] == ' ')
-	{
-		i++;
-		if (i >= u32BufSize)
-		{
-			return 0;
-		}
-	}
-
-	while ((szStr[i] != '\0') && (i < u32BufSize))
-	{
-		if ((szStr[i] >= '0') && (szStr[i] <= '9'))
-		{
-			uint32_t tmp = (uint32_t)(szStr[i] - '0');
-			u32Val *= u32Base;
-			u32Val += tmp;
-		}
-		else if ((szStr[i] >= 'a') && (szStr[i] <= 'f'))
-		{
-			uint32_t tmp = (uint32_t)(szStr[i] - 'a') + 10u;
-			u32Val *= u32Base;
-			u32Val += tmp;
-		}
-		else if ((szStr[i] >= 'A') && (szStr[i] <= 'F'))
-		{
-			uint32_t tmp = (uint32_t)(szStr[i] - 'A') + 10u;
-			u32Val *= u32Base;
-			u32Val += tmp;
-		}
-		else
-		{
-			/* NOP */
-		}
-		i++;
-	}
-
-	return u32Val;
-}
+extern uint32_t mimic_strtoul(const TCHAR szStr[], const uint32_t u32BufSize, const uint32_t u32Base);
 
 /**
  * @brief strtoull
@@ -1190,53 +628,7 @@ static inline uint32_t mimic_strtoul(const TCHAR szStr[], const uint32_t u32BufS
  * @param [in] u32BufSize sizeof(szStr) (!= 0)
  * @return uint64_t
  */
-static inline uint64_t mimic_strtoull(const TCHAR szStr[], const uint32_t u32BufSize, const uint32_t u32Base)
-{
-	uint64_t u64Val = 0;
-	uint32_t i = 0;
-	if ((szStr == NULL) || (u32BufSize == 0u) || ((u32Base != 10u) && (u32Base != 16u)))
-	{
-		return 0;
-	}
-	while (szStr[i] == ' ')
-	{
-		i++;
-		if (i >= u32BufSize)
-		{
-			return 0;
-		}
-	}
-
-	while ((szStr[i] != '\0') && (i < u32BufSize))
-	{
-		if ((szStr[i] >= '0') && (szStr[i] <= '9'))
-		{
-			uint64_t tmp = (uint64_t)(szStr[i] - '0');
-			u64Val *= u32Base;
-			u64Val += tmp;
-		}
-		else if ((szStr[i] >= 'a') && (szStr[i] <= 'f'))
-		{
-			uint64_t tmp = (uint64_t)(szStr[i] - 'a') + 10u;
-			u64Val *= u32Base;
-			u64Val += tmp;
-		}
-		else if ((szStr[i] >= 'A') && (szStr[i] <= 'F'))
-		{
-			uint64_t tmp = (uint64_t)(szStr[i] - 'A') + 10u;
-			u64Val *= u32Base;
-			u64Val += tmp;
-		}
-		else
-		{
-			/* NOP */
-		}
-		i++;
-	}
-
-	return u64Val;
-}
-
+extern uint64_t mimic_strtoull(const TCHAR szStr[], const uint32_t u32BufSize, const uint32_t u32Base);
 /*@} end of group MIMICLIB */
 
 #ifdef __cplusplus
